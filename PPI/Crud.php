5d260c0dc2bee9937db299a59453b1f35487c0a0
Paul Dragoonis <dragoonis@gmail.com>
2011-10-27 22:16:11 +0100

    Adding the converter to create PPI\Form elements and their rules into a 'formstructure' which can be manipulated and traversed

--- PPI/Crud.php
+++ PPI/Crud.php
@@ -3,22 +3,51 @@
  * @author    Paul Dragoonis <dragoonis@php.net>
  * @license   http://opensource.org/licenses/mit-license.php MIT
  * @package   CRUD
+ * @link      http://www.ppi.io
  */
-class PPI_Crud {
+namespace PPI;
+class Crud {
+	
 	/**
 	 * This function is used to parse/load a yaml file.
 	 *
-	 * @param $file
-	 * @return yaml parsed
+	 * @param string $formName
+	 * @return array
 	 */
-	function getFormStructure($file) {
-		$file = CONFIGPATH.'Forms/'.$file.".yaml";
+	function getFormStructure($formName) {
+		$path = CONFIGPATH . 'Forms/' . $formName . ".yaml";
 		if(extension_loaded('yaml')) {
 			// do stuff here -- with yaml
-			return yaml_parse_file($file);
+			return yaml_parse_file($path);
 		} else {
+
+			include_once(VENDORPATH . 'Spyc/spyc.php');
+			
+			$form = new \PPI\Form();
+			
 			// use spyc
-			return Spyc::YAMLLoad($file);
+			$structure = \Spyc::YAMLLoad($path);
+			$fields = array();
+			if(!empty($structure['fields'])) {
+				foreach($structure['fields'] as $fieldName => $field) {
+					
+					$fieldType = $field['type'];
+					$fieldLabel = $field['label'];
+					unset($field['type'], $field['label']);
+					$fields[$fieldName] = $form->$fieldType($fieldName, $field);
+				}
+				
+				foreach($structure['rules'] as $fieldName => $rule) {
+					
+					if(isset($fields[$fieldName])) {
+						$ruleValue = isset($rule['value']) ? $rule['value'] : null;
+						$fields[$fieldName]->setRule($rule['message'], $rule['type'], $ruleValue);
+					}
+					
+				}
+			}
+			return $fields;
 		}
 	}
+	
 }
