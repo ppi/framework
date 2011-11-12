d47aeabdf352c2f84e39055e2d7444840ceaca71
Paul Dragoonis <dragoonis@gmail.com>
2011-11-12 20:34:23 +0000

    Exception handler improvements. coding standards mods

--- PPI/Core/ExceptionHandler.php
+++ PPI/Core/ExceptionHandler.php
@@ -1,27 +1,27 @@
 <?php
 /**
- * @author    Ben Tadiar <ben@handcraftedbyben.co.uk>
+ * @author    Paul Dragoonis (dragoonis@php.net)
  * @license   http://opensource.org/licenses/mit-license.php MIT
  * @package   Core
- * @link      wwww.ppiframework.com
+ * @link      wwww.ppi.io
  */
 namespace PPI\Core;
 class ExceptionHandler {
 	
 	/**
 	 * PPI Exception handler
-	 * The try/catch block will prevent a fatal error if
-	 * an exception is thrown within the handler itself
+	 * The try/catch block will prevent a fatal error if an exception is thrown within the handler itself
+	 * 
 	 * @param object $e Exception object
 	 */
-	public static function handle($e)
-	{
+	public static function handle($e) {
+		
 		try {
 			// We can log exceptions, email admins etc as required here
 			$trace = $e->getTrace();
 			require(SYSTEMPATH  . 'View' . DS . 'Exception.php');
-		}
-		catch(Exception $e){
+
+		} catch(Exception $e){
 			require(SYSTEMPATH  . 'View' . DS . 'Exception.php');
 		}
 		exit;
