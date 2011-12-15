fae397122575ab310a554a2f150cfcb144c8c265
Paul Dragoonis <dragoonis@gmail.com>
2011-12-15 04:09:50 +0000

    Coding standards fixing. phpdoc improvements. Namespace mods

--- PPI/Cache.php
+++ PPI/Cache.php
@@ -15,16 +15,16 @@ class Cache {
 	 *
 	 * @var array
 	 */
-    protected $_defaults = array(
-        'handler'  => 'disk'
-    );
+	protected $_defaults = array(
+		'handler'  => 'disk'
+	);
 
-    /**
-     * The handler in use
-     *
-     * @var null|PPI_Cache_Interface
-     */
-    protected $_handler = null;
+	/**
+	 * The handler in use
+	 *
+	 * @var null|PPI\Cache\CacheInterface
+	 */
+	protected $_handler = null;
 
 	/**
 	 * The options to the cache layer. This can be an array of options
@@ -39,80 +39,80 @@ class Cache {
 			$options = array('handler' => $options);
 		}
 
-        if(isset($options['handler'])) {
-            // If it's a pre instantiated cache handler then use that
-            if(!is_string($options['handler']) && $options['handler'] instanceof \PPI\Cache\CacheInterface) {
-                $this->_handler = $options['handler'];
-                unset($options['handler']);
-            }
-        }
+		if(isset($options['handler'])) {
+			// If it's a pre instantiated cache handler then use that
+			if(!is_string($options['handler']) && $options['handler'] instanceof \PPI\Cache\CacheInterface) {
+				$this->_handler = $options['handler'];
+				unset($options['handler']);
+			}
+		}
 
-        $this->_defaults = ($options + $this->_defaults);
+		$this->_defaults = ($options + $this->_defaults);
 
-        // If no handler was passed in, then we setup that handler now by the string name: i.e: 'disk'
-        if($this->_handler === null) {
-            $this->setupHandler($this->_defaults['handler']);
-        }
+		// If no handler was passed in, then we setup that handler now by the string name: i.e: 'disk'
+		if($this->_handler === null) {
+			$this->setupHandler($this->_defaults['handler']);
+		}
 	}
 
 	/**
 	 * Initialise the cache handler
-     *
+	 *
 	 * @param string $handler The handler name
 	 * @return void
 	 * @throws \PPI\Core\CoreException
 	 */
 	function setupHandler($handler) {
 		$handler = strtolower($handler);
-        $handler = 'PPI\\Cache\\' . ucfirst($handler);
+		$handler = 'PPI\\Cache\\' . ucfirst($handler);
 		$this->_handler = new $handler($this->_defaults);
-        if($this->_handler->enabled() === false) {
-            throw new \PPI\Core\CoreException('The cache driver ' . $handler . ' is currently disabled.');
-        }
+		if($this->_handler->enabled() === false) {
+			throw new \PPI\Core\CoreException('The cache driver ' . $handler . ' is currently disabled.');
+		}
 		$this->_handler->init();
 	}
 
-    /**
-     * Get a key value from the cache
-     *
-     * @param string $key The Key
-     * @return mixed
-     */
-    function get($key) {
-    	return $this->_handler->get($key);
-    }
+	/**
+	 * Get a key value from the cache
+	 *
+	 * @param string $key The Key
+	 * @return mixed
+	 */
+	function get($key) {
+		return $this->_handler->get($key);
+	}
 
-    /**
-     * Set a value in the cache
-     *
-     * @param string $key The Key
-     * @param mixed $value The Value
-     * @param integer $ttl The TTL
-     * @return boolean
-     */
-    function set($key, $value, $ttl = 0) {
-    	return $this->_handler->set($key, $value, $ttl);
-    }
+	/**
+	 * Set a value in the cache
+	 *
+	 * @param string $key The Key
+	 * @param mixed $value The Value
+	 * @param integer $ttl The TTL
+	 * @return boolean
+	 */
+	function set($key, $value, $ttl = 0) {
+		return $this->_handler->set($key, $value, $ttl);
+	}
 
-    /**
-     * Check if a key exists in the cache
-     *
-     * @param string $key The Key
-     * @return boolean
-     */
-    function exists($key) {
-    	return $this->_handler->exists($key);
-    }
+	/**
+	 * Check if a key exists in the cache
+	 *
+	 * @param string $key The Key
+	 * @return boolean
+	 */
+	function exists($key) {
+		return $this->_handler->exists($key);
+	}
 
-    /**
-     * Remove a value from the cache by key
-     *
-     * @param string $key The Key
-     * @return boolean
-     */
-    function remove($key) {
-    	return $this->_handler->remove($key);
-    }
+	/**
+	 * Remove a value from the cache by key
+	 *
+	 * @param string $key The Key
+	 * @return boolean
+	 */
+	function remove($key) {
+		return $this->_handler->remove($key);
+	}
 
 	/**
 	 * Get the current registered handler
@@ -124,17 +124,17 @@ class Cache {
 	}
 
 	/**
-	 * Set the current handler, we check enabled() and exec init() on the handler too to make sure it's ready to go.
+	 * Set the current handler.
 	 *
 	 * @throws \PPI\Core\CoreException
-	 * @param PPI_Cache_Interface $handler
+	 * @param PPI\Cache\CacheInterface $handler
 	 * @return void
 	 */
-	function setHandler(PPI_Cache_Interface $handler) {
+	function setHandler(PPI\Cache\CacheInterface $handler) {
 		$this->_handler = $handler;
-        if($this->_handler->enabled() === false) {
-            throw new \PPI\Core\CoreException('The cache driver ' . get_class($handler) . ' is currently disabled.');
-        }
+		if($this->_handler->enabled() === false) {
+			throw new \PPI\Core\CoreException('The cache driver ' . get_class($handler) . ' is currently disabled.');
+		}
 		$this->_handler->init();
 	}
 
