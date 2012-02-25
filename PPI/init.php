745199ee3472f5b85d2185340349ecc40baeaff3
Paul Dragoonis <dragoonis@gmail.com>
2012-02-25 20:51:30 +0000

    Setting up a classmap autoloader. Sucessfully autoloading Zend, Symfony and PPI.

--- PPI/init.php
+++ PPI/init.php
@@ -10,22 +10,21 @@ defined('PPI_VERSION') || define('PPI_VERSION', '2.0');
 defined('DS') || define('DS', DIRECTORY_SEPARATOR);
 defined('PPI_PATH') || define('PPI_PATH', realpath(__DIR__) . '/');
 defined('PPI_VENDOR_PATH') || define('PPI_VENDOR_PATH', dirname(PPI_PATH) . '/Vendor/');
+
 // Autoload registration
 require 'Autoload.php';
+require PPI_VENDOR_PATH . 'Zend/Loader/ClassMapAutoloader.php';
+require PPI_VENDOR_PATH . 'Zend/Loader/StandardAutoloader.php';
 use PPI\Autoload;
 
+$mapLoader = new \Zend\Loader\ClassMapAutoloader();
+$mapLoader->registerAutoloadMap(PPI_PATH . 'autoload_classmap.php');
+$mapLoader->register();
+
 Autoload::config(array(
-	'loaderClassPath' => PPI_VENDOR_PATH . 'Symfony/Component/ClassLoader/UniversalClassLoader.php',
-	'loaderClass'     => '\Symfony\Component\ClassLoader\UniversalClassLoader'
+	'loader'    => new \Zend\Loader\StandardAutoloader(),
+	'mapLoader' => $mapLoader
 ));
-Autoload::add('PPI', dirname(PPI_PATH));
-Autoload::add('Symfony', dirname(PPI_VENDOR_PATH . 'Symfony'));
-Autoload::add('Zend', dirname(PPI_VENDOR_PATH . 'Zend'));
+Autoload::add('PPI', PPI_PATH);
+Autoload::add('Symfony', PPI_VENDOR_PATH . 'Symfony');
 Autoload::register();
-
-
-// General stuff
-//require 'common.php';
-// load up custom error handlers
-//require 'errors.php';
-//setErrorHandlers('ppi_error_handler', 'ppi_exception_handler');
