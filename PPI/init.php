65df79681e11f9ef5164f87f016e0cb41832fb82
Paul Dragoonis <dragoonis@gmail.com>
2012-02-25 22:15:26 +0000

    Changing autoloading to use SF2 instead of ZF2 due to speed differences

--- PPI/init.php
+++ PPI/init.php
@@ -13,18 +13,17 @@ defined('PPI_VENDOR_PATH') || define('PPI_VENDOR_PATH', dirname(PPI_PATH) . '/Ve
 
 // Autoload registration
 require 'Autoload.php';
-require PPI_VENDOR_PATH . 'Zend/Loader/ClassMapAutoloader.php';
-require PPI_VENDOR_PATH . 'Zend/Loader/StandardAutoloader.php';
+require PPI_VENDOR_PATH . 'Symfony/Component/ClassLoader/MapClassLoader.php';
+require PPI_VENDOR_PATH . 'Symfony/Component/ClassLoader/UniversalClassLoader.php';
 use PPI\Autoload;
 
-$mapLoader = new \Zend\Loader\ClassMapAutoloader();
-$mapLoader->registerAutoloadMap(PPI_PATH . 'autoload_classmap.php');
+$mapLoader = new \Symfony\Component\ClassLoader\MapClassLoader(include_once(PPI_PATH . 'autoload_classmap.php'));
 $mapLoader->register();
 
 Autoload::config(array(
-	'loader'    => new \Zend\Loader\StandardAutoloader(),
+	'loader'    => new \Symfony\Component\ClassLoader\UniversalClassLoader(),
 	'mapLoader' => $mapLoader
 ));
 Autoload::add('PPI', PPI_PATH);
-Autoload::add('Symfony', PPI_VENDOR_PATH . 'Symfony');
+Autoload::add('Symfony', PPI_VENDOR_PATH);
 Autoload::register();
