601778916d5fa4ec128f0ab659d15c934a862880
Paul Dragoonis <dragoonis@gmail.com>
2012-03-17 14:48:26 +0000

    Adding templating functionality to the modules

---
+++ PPI/Module/Templating/FileSystemLoader.php
@@ -0,0 +1,66 @@
+<?php
+
+
+namespace PPI\Module\Templating;
+
+use Symfony\Component\Templating\Storage\FileStorage;
+use Symfony\Component\Templating\Loader\LoaderInterface;
+use Symfony\Component\Config\FileLocatorInterface;
+use Symfony\Component\Templating\TemplateReferenceInterface;
+
+/**
+ * FilesystemLoader is a loader that read templates from the filesystem.
+ *
+ */
+class FileSystemLoader implements LoaderInterface
+{
+    protected $locator;
+
+    /**
+     * Constructor.
+     *
+     * @param FileLocatorInterface $locator A FileLocatorInterface instance
+     */
+    public function __construct(FileLocatorInterface $locator)
+    {
+        $this->locator = $locator;
+    }
+
+    /**
+     * Loads a template.
+     *
+     * @param TemplateReferenceInterface $template A template
+     *
+     * @return Storage|Boolean false if the template cannot be loaded, a Storage instance otherwise
+     */
+    public function load(TemplateReferenceInterface $template)
+    {
+        try {
+            $file = $this->locator->locate($template);
+			
+        } catch (\InvalidArgumentException $e) {
+            return false;
+        }
+
+        return new FileStorage($file);
+    }
+
+    /**
+     * Returns true if the template is still fresh.
+     *
+     * @param TemplateReferenceInterface $template The template name as an array
+     * @param integer                    $time     The last modification time of the cached template (timestamp)
+     */
+    public function isFresh(TemplateReferenceInterface $template, $time)
+    {
+        if (false === $storage = $this->load($template)) {
+            return false;
+        }
+
+        if (!is_readable((string) $storage)) {
+            return false;
+        }
+
+        return filemtime((string) $storage) < $time;
+    }
+}
