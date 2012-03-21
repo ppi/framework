7c06319484107f68c13f68d15beb8eea12beb9a3
Paul Dragoonis <dragoonis@gmail.com>
2012-03-21 02:16:27 +0000

    Controller development. - Now have a url generator to reverse route key names to an actual url - now able to perform redirects using the url generator - Fixing a bug in Module.php, caching the generated routes. - Now have Controller->getService() - Now have Controller->redirect() - Now have Controller->generateUrl()

--- PPI/Module/Controller.php
+++ PPI/Module/Controller.php
@@ -1,21 +1,9 @@
 <?php
 namespace PPI\Module;
 
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
 class Controller {
-	
-	/**
-	 * The controller's request object
-	 * 
-	 * @var null
-	 */
-	protected $_request = null;
-	
-	/**
-	 * The controllers' response object
-	 * 
-	 * @var null
-	 */
-	protected $_response = null;
 	
 	/**
 	 * Service Locator
@@ -27,25 +15,77 @@ class Controller {
 	/**
 	 * Get the request object
 	 * 
-	 * @return null
+	 * @return object
 	 */
 	function getRequest() {
 		return $this->_serviceLocator->get('request');
 	}
 	
 	/**
-	 * Get thre response object
+	 * Get the response object
 	 * 
-	 * @return null
+	 * @return object
 	 */
 	function getResponse() {
 		return $this->_serviceLocator->get('response');
 	}
 	
 	/**
+	 * Get a query string value
+	 * 
+	 * @param string $key
+	 */
+	function queryString($key) {
+		
+	}
+	
+	/**
+	 * Get a post value
+	 * 
+	 * @param string $key
+	 */
+	function post($key) {
+		
+	}
+	
+	/**
+	 * Get a server value
+	 * 
+	 * @param string $key
+	 * @return void
+	 */
+	function server($key) {
+		
+	}
+	
+	/**
+	 * Get/Set a session value
+	 * 
+	 * @param string $key
+	 * @param null|mixed $val If this is not null, it enters setter mode
+	 */
+	function session($key, $val = null) {
+		$session = $this->_serviceLocator->get('request')->getSession();
+	}
+	
+	/**
+	 * Check if a condition 'is' true.
+	 * 
+	 * @param $key
+	 */
+	function is($key) {
+		
+	}
+	
+	function getRemote($key) {
+		
+	}
+	
+	/**
 	 * Set the service locator
 	 * 
 	 * @param object $locator
+	 * @return void
 	 */
 	function setServiceLocator($locator) {
 		$this->_serviceLocator = $locator;
@@ -60,7 +100,20 @@ class Controller {
 		return $this->_serviceLocator;
 	}
 	
+	function getService($service) {
+		return $this->getServiceLocator()->get($service);
+	}
+	
+	/**
+	 * Render a template
+	 * 
+	 * @param string $template The template to render
+	 * @param array $params The params to pass to the renderer
+	 * @param array $options Extra options
+	 * @return string
+	 */
 	protected function render($template, array $params = array(), array $options = array()) {
+		
 		$renderer = $this->_serviceLocator->get('templating');
 		
 		// Helpers
@@ -72,6 +125,39 @@ class Controller {
 		
 		$params['view'] = $renderer;
 		return $renderer->render($template, $params);
+	}
+	
+	/**
+	 * Set Flash Message
+	 * 
+	 * @param string $flashType
+	 * @param string $message
+	 */
+	protected function setFlash($flashType, $message) {
+		$this->getSession()->setFlash($flashType, $message);
+	}
+	
+	/**
+	 * Create a RedirectResponse object with your $url and $statusCode
+	 * 
+	 * @param string $url
+	 * @param int $statusCode
+	 * @return void
+	 */
+	protected function redirect($url, $statusCode = 302) {
+		$this->getServiceLocator()->set('response', new RedirectResponse($url, $statusCode));
+	}
+	
+	/**
+	 * Generate a URL from the specified route name
+	 * 
+	 * @param string $route
+	 * @param array $parameters
+	 * @param bool $absolute
+	 * @return string
+	 */
+	protected function generateUrl($route, $parameters = array(), $absolute = false) {
+		return $this->getService('url.generator')->generate($route, $parameters, $absolute);
 	}
 	
 }
