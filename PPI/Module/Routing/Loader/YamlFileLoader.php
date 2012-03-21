c7aa3234c85bae8327fd366173d5850580169bc0
Paul Dragoonis <dragoonis@gmail.com>
2012-03-21 02:16:27 +0000

    Controller development. - Now have a url generator to reverse route key names to an actual url - now able to perform redirects using the url generator - Fixing a bug in Module.php, caching the generated routes. - Now have Controller->getService() - Now have Controller->redirect() - Now have Controller->generateUrl()

---
+++ PPI/Module/Routing/Loader/YamlFileLoader.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace PPI\Module\Routing\Loader;
+	
+use Symfony\Component\Routing\Loader\YamlFileLoader as BaseYamlFileLoader,
+	Symfony\Component\Routing\RouteCollection;
+
+class YamlFileLoader extends BaseYamlFileLoader {
+	
+	protected $_defaults = array();
+	
+	public function setDefaults($defaults) {
+		$this->_defaults = $defaults;
+	}
+	
+	protected function parseRoute(RouteCollection $collection, $name, $config, $file) {
+		
+		if(!empty($this->_defaults)) {
+			$config['defaults'] = array_merge($config['defaults'], $this->_defaults);
+		}
+		
+		parent::parseRoute($collection, $name, $config, $file);
+	}
+	
+}
