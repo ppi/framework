bab0d07db4b3ddf7feab15e4f8456c96b06a35ab
Vítor Brandão <noisebleed@noiselabs.org>
2012-07-01 23:48:53 +0100

    Code fix to make PPI PSR-1 and PSR-2 compliant
    - Apply fabpot's php-cs-fixer to make PPI code PSR-1 and PSR-2 compliant
    - Remove executable bit from README.md and some .php source files

--- PPI/Module/Listener/DefaultListenerAggregate.php
+++ PPI/Module/Listener/DefaultListenerAggregate.php
@@ -2,95 +2,100 @@
 namespace PPI\Module\Listener;
 
 use
-	Zend\ModuleManager\Listener\DefaultListenerAggregate as ZendDefaultListenerAggregate,
-	Zend\EventManager\EventManagerInterface,
-	Zend\ModuleManager\ModuleEvent;
+    Zend\ModuleManager\Listener\DefaultListenerAggregate as ZendDefaultListenerAggregate,
+    Zend\EventManager\EventManagerInterface,
+    Zend\ModuleManager\ModuleEvent;
 
-class DefaultListenerAggregate extends ZendDefaultListenerAggregate {
-	
-	/**
-	 * The routes registered for our 
-	 * 
-	 * @var array
-	 */
-	protected $_routes = array();
-	
-	/**
-	 * Services for the ServiceLocator
-	 * 
-	 * @var array
-	 */
-	protected $_services = array();
-	
-	/**
-	 * Override of attach(). Customising the events to be triggered upon the 'loadModule' event.
-	 * 
-	 * @param \Zend\EventManager\EventManagerInterface $events
-	 * @return DefaultListenerAggregate
-	 */
-	public function attach(EventManagerInterface $events) {
+class DefaultListenerAggregate extends ZendDefaultListenerAggregate
+{
+    /**
+     * The routes registered for our
+     *
+     * @var array
+     */
+    protected $_routes = array();
 
-		parent::attach($events);
-		
-		$options = $this->getOptions();
-		
-		// This process can be expensive and affect perf if enabled. So we have the flexability to skip it.
-		if($options->routingEnabled) {
-			$this->listeners[] = $events->attach('loadModule', array($this, 'routesTrigger'), 3000);
-		}
-		$this->listeners[] = $events->attach('loadModule', array($this, 'initServicesTrigger'), 4000);
-		return $this;
+    /**
+     * Services for the ServiceLocator
+     *
+     * @var array
+     */
+    protected $_services = array();
 
-	}
-	
-	/**
-	 * Event callback for 'routesTrigger'
-	 * 
-	 * @param \Zend\ModuleManager\ModuleEvent $e
-	 * @return DefaultListenerAggregate
-	 */
-	public function routesTrigger(ModuleEvent $e) {
+    /**
+     * Override of attach(). Customising the events to be triggered upon the 'loadModule' event.
+     *
+     * @param  \Zend\EventManager\EventManagerInterface $events
+     * @return DefaultListenerAggregate
+     */
+    public function attach(EventManagerInterface $events)
+    {
+        parent::attach($events);
 
-		$module = $e->getParam('module');
-		if(is_callable(array($module, 'getRoutes'))) {
-			$this->_routes[$e->getParam('moduleName')] = $module->getRoutes();
-		}
-		return $this;
+        $options = $this->getOptions();
 
-	}
-	
-	/**
-	 * Event callback for 'initServicesTrigger'
-	 * 
-	 * @param \Zend\ModuleManager\ModuleEvent $e
-	 * @return DefaultListenerAggregate
-	 */
-	public function initServicesTrigger(ModuleEvent $e) {
+        // This process can be expensive and affect perf if enabled. So we have the flexability to skip it.
+        if ($options->routingEnabled) {
+            $this->listeners[] = $events->attach('loadModule', array($this, 'routesTrigger'), 3000);
+        }
+        $this->listeners[] = $events->attach('loadModule', array($this, 'initServicesTrigger'), 4000);
 
-		$module = $e->getParam('module');
-		if(is_callable(array($module, 'initServices'))) {
-			$this->_services = array_merge($this->_services, $module->initServices($this->_services));
-		}
-		return $this;
+        return $this;
 
-	}
-	
-	/**
-	 * Get the registered routes
-	 * 
-	 * @return array
-	 */
-	public function getRoutes() {
-		return $this->_routes;
-	}
-	
-	/**
-	 * Get the registered services
-	 * 
-	 * @return mixed
-	 */
-	public function getServices() {
-		return $this->_services;
-	}
+    }
+
+    /**
+     * Event callback for 'routesTrigger'
+     *
+     * @param  \Zend\ModuleManager\ModuleEvent $e
+     * @return DefaultListenerAggregate
+     */
+    public function routesTrigger(ModuleEvent $e)
+    {
+        $module = $e->getParam('module');
+        if (is_callable(array($module, 'getRoutes'))) {
+            $this->_routes[$e->getParam('moduleName')] = $module->getRoutes();
+        }
+
+        return $this;
+
+    }
+
+    /**
+     * Event callback for 'initServicesTrigger'
+     *
+     * @param  \Zend\ModuleManager\ModuleEvent $e
+     * @return DefaultListenerAggregate
+     */
+    public function initServicesTrigger(ModuleEvent $e)
+    {
+        $module = $e->getParam('module');
+        if (is_callable(array($module, 'initServices'))) {
+            $this->_services = array_merge($this->_services, $module->initServices($this->_services));
+        }
+
+        return $this;
+
+    }
+
+    /**
+     * Get the registered routes
+     *
+     * @return array
+     */
+    public function getRoutes()
+    {
+        return $this->_routes;
+    }
+
+    /**
+     * Get the registered services
+     *
+     * @return mixed
+     */
+    public function getServices()
+    {
+        return $this->_services;
+    }
 
 }
