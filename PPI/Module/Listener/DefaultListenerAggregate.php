160d5f0d7eaeb2d6f8766f134c15b6f32758259c
Paul Dragoonis <dragoonis@gmail.com>
2012-08-22 14:59:48 +0100

    [Module] Updating our DefaultListenerAggregate() to be compliant with the latest ZF2 code. Having it pull services from modules.

--- PPI/Module/Listener/DefaultListenerAggregate.php
+++ PPI/Module/Listener/DefaultListenerAggregate.php
@@ -1,10 +1,17 @@
 <?php
 namespace PPI\Module\Listener;
 
-use
-    Zend\ModuleManager\Listener\DefaultListenerAggregate as ZendDefaultListenerAggregate,
-    Zend\EventManager\EventManagerInterface,
-    Zend\ModuleManager\ModuleEvent;
+use Zend\Loader\ModuleAutoloader;
+use PPI\ServiceManager\ServiceManager;
+use Zend\EventManager\EventManagerInterface;
+
+use Zend\Stdlib\ArrayUtils;
+
+use Zend\ModuleManager\ModuleEvent;
+use Zend\ModuleManager\Listener\InitTrigger;
+use Zend\ModuleManager\Listener\ModuleResolverListener;
+use Zend\ModuleManager\Listener\AutoloaderListener;
+use Zend\ModuleManager\Listener\DefaultListenerAggregate as ZendDefaultListenerAggregate;
 
 class DefaultListenerAggregate extends ZendDefaultListenerAggregate
 {
@@ -23,6 +30,22 @@ class DefaultListenerAggregate extends ZendDefaultListenerAggregate
     protected $_services = array();
 
     /**
+     * The Service Manager
+     * 
+     * @var
+     */
+    protected $_serviceManager;
+
+    /**
+     * Set the service manager
+     * 
+     * @param \PPI\ServiceManager\ServiceManager $sm
+     */
+    public function setServiceManager(ServiceManager $sm) {
+        $this->_serviceManager = $sm;
+    }
+
+    /**
      * Override of attach(). Customising the events to be triggered upon the 'loadModule' event.
      *
      * @param  \Zend\EventManager\EventManagerInterface $events
@@ -30,15 +53,24 @@ class DefaultListenerAggregate extends ZendDefaultListenerAggregate
      */
     public function attach(EventManagerInterface $events)
     {
-        parent::attach($events);
 
-        $options = $this->getOptions();
+        $options                     = $this->getOptions();
+        $configListener              = $this->getConfigListener();
+        $moduleAutoloader            = new ModuleAutoloader($options->getModulePaths());
+
+        // High priority, we assume module autoloading (for FooNamespace\Module classes) should be available before anything else
+        $this->listeners[] = $events->attach(ModuleEvent::EVENT_LOAD_MODULES, array($moduleAutoloader, 'register'), 9000);
+        $this->listeners[] = $events->attach(ModuleEvent::EVENT_LOAD_MODULE_RESOLVE, new ModuleResolverListener);
+        // High priority, because most other loadModule listeners will assume the module's classes are available via autoloading
+        $this->listeners[] = $events->attach(ModuleEvent::EVENT_LOAD_MODULE, new AutoloaderListener($options), 9000);
+        $this->listeners[] = $events->attach(ModuleEvent::EVENT_LOAD_MODULE, new InitTrigger($options));
+        $this->listeners[] = $events->attach($configListener);
 
         // This process can be expensive and affect perf if enabled. So we have the flexability to skip it.
         if ($options->routingEnabled) {
             $this->listeners[] = $events->attach('loadModule', array($this, 'routesTrigger'), 3000);
         }
-        $this->listeners[] = $events->attach('loadModule', array($this, 'initServicesTrigger'), 4000);
+        $this->listeners[] = $events->attach('loadModule', array($this, 'getServicesTrigger'), 3000);
 
         return $this;
 
@@ -52,9 +84,9 @@ class DefaultListenerAggregate extends ZendDefaultListenerAggregate
      */
     public function routesTrigger(ModuleEvent $e)
     {
-        $module = $e->getParam('module');
+        $module = $e->getModule();
         if (is_callable(array($module, 'getRoutes'))) {
-            $this->_routes[$e->getParam('moduleName')] = $module->getRoutes();
+            $this->_routes[$e->getModuleName()] = $module->getRoutes();
         }
 
         return $this;
@@ -67,11 +99,14 @@ class DefaultListenerAggregate extends ZendDefaultListenerAggregate
      * @param  \Zend\ModuleManager\ModuleEvent $e
      * @return DefaultListenerAggregate
      */
-    public function initServicesTrigger(ModuleEvent $e)
+    public function getServicesTrigger(ModuleEvent $e)
     {
-        $module = $e->getParam('module');
-        if (is_callable(array($module, 'initServices'))) {
-            $this->_services = array_merge($this->_services, $module->initServices($this->_services));
+        $module = $e->getModule();
+        if (is_callable(array($module, 'getServiceConfig'))) {
+            $services = $module->getServiceConfig();
+            if(isset($services['factories'])) {
+                $this->_services[$e->getModuleName()] = $services['factories'];
+            }
         }
 
         return $this;
@@ -95,7 +130,11 @@ class DefaultListenerAggregate extends ZendDefaultListenerAggregate
      */
     public function getServices()
     {
-        return $this->_services;
+        $mergedModuleServices = array();
+        foreach($this->_services as $services) {
+            $mergedModuleServices = ArrayUtils::merge($mergedModuleServices, $services);
+        }
+        return $mergedModuleServices;
     }
 
 }
