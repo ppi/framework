d12b8dd9da9aa06ad95b968c7118bf02dd6c24f4
Paul Dragoonis <dragoonis@gmail.com>
2012-08-24 02:17:55 +0100

    Adding in the exception component. Basic fatal error page shown. Still need to dispatch the exception to the Framework module and perform a render, upon failure of that revert back to the fatal

--- PPI/Exception/Handler.php
+++ PPI/Exception/Handler.php
@@ -1,11 +1,17 @@
 <?php
+
 /**
- * @author    Paul Dragoonis (dragoonis@php.net)
- * @license   http://opensource.org/licenses/mit-license.php MIT
- * @package   Core
- * @link      wwww.ppi.io
+ * This file is part of the PPI Framework.
+ *
+ * @category    PPI
+ * @package     Core
+ * @copyright   Copyright (c) 2012 Paul Dragoonis <paul@ppi.io>
+ * @license     http://opensource.org/licenses/mit-license.php MIT
+ * @link        http://www.ppi.io
  */
+
 namespace PPI\Exception;
+
 class Handler {
 	
 	/**
