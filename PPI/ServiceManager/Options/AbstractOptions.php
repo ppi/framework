b6388b922e4259d2fb56a5cbbf977b7453b74580
Vítor Brandão <noisebleed@noiselabs.org>
2012-08-25 23:39:20 +0100

    Implements \ArrayAccess, \IteratorAggregate, \Countable in AbstractOptions

--- PPI/ServiceManager/Options/AbstractOptions.php
+++ PPI/ServiceManager/Options/AbstractOptions.php
@@ -21,6 +21,91 @@ use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
  *
  * @author Vítor Brandão <vitor@ppi.io>
  */
-abstract class AbstractOptions extends ParameterBag implements OptionsInterface
+abstract class AbstractOptions extends ParameterBag implements OptionsInterface, \ArrayAccess, \IteratorAggregate, \Countable
 {
+    /**
+     * Equivalent to {@link has()}.
+     *
+     * @param string $option The option name.
+     *
+     * @return Boolean Whether the option exists.
+     *
+     * @see \ArrayAccess::offsetExists()
+     */
+    public function offsetExists($option)
+    {
+        return $this->parameters->has($option);
+    }
+
+    /**
+     * Equivalent to {@link get()}.
+     *
+     * @param string $option The option name.
+     *
+     * @return mixed The option value.
+     *
+     * @throws \OutOfBoundsException     If the option does not exist.
+     * @throws OptionDefinitionException If a cyclic dependency is detected
+     *                                   between two lazy options.
+     *
+     * @see \ArrayAccess::offsetGet()
+     */
+    public function offsetGet($option)
+    {
+        return $this->parameters->get($option);
+    }
+
+    /**
+     * Equivalent to {@link set()}.
+     *
+     * @param string $option The name of the option.
+     * @param mixed  $value  The value of the option. May be a closure with a
+     *                       signature as defined in DefaultOptions::add().
+     *
+     * @throws OptionDefinitionException If options have already been read.
+     *                                   Once options are read, the container
+     *                                   becomes immutable.
+     *
+     * @see \ArrayAccess::offsetSet()
+     */
+    public function offsetSet($option, $value)
+    {
+        $this->parameters->set($option, $value);
+    }
+
+    /**
+     * Equivalent to {@link remove()}.
+     *
+     * @param string $option The option name.
+     *
+     * @throws OptionDefinitionException If options have already been read.
+     *                                   Once options are read, the container
+     *                                   becomes immutable.
+     *
+     * @see \ArrayAccess::offsetUnset()
+     */
+    public function offsetUnset($option)
+    {
+        $this->parameters->remove($option);
+    }
+
+   /**
+     * Returns an iterator for parameters.
+     *
+     * @return \ArrayIterator An \ArrayIterator instance
+     */
+    public function getIterator()
+    {
+        return new \ArrayIterator($this->parameters->all());
+    }
+
+    /**
+     * Returns the number of parameters.
+     *
+     * @return int The number of parameters
+     */
+    public function count()
+    {
+        return count($this->parameters->all());
+    }
 }
