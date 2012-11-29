9137b44b6c1bb58c79473e903c8aecdecf31913a
Blaine Schmeisser <blaine.schmeisser@vitals.com>
2012-11-28 20:57:06 -0600

    Add basic bootstrap/autoload information for phpunit

---
+++ PPI/Test/Bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This file is part of the PPI Framework.
+ *
+ * @copyright  Copyright (c) 2012 Paul Dragoonis <paul@ppi.io>
+ * @license    http://opensource.org/licenses/mit-license.php MIT
+ * @link       http://www.ppi.io
+ */
+namespace PPI\Test;
+
+require_once(__DIR__ . '/../AutoLoad.php');
+require_once(__DIR__ . '/AutoLoad.php');
+
+\PPI\Autoload::config(array(
+    'loader'    => new Autoload,
+));
+\PPI\Autoload::register();
