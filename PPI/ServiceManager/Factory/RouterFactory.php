23e4b1d31a8f348354cd92a588c116cd0be00f99
Vítor Brandão <noisebleed@noiselabs.org>
2012-12-17 00:23:37 +0000

    Add RouterFactory and RoutingHelperFactory

---
+++ PPI/ServiceManager/Factory/RouterFactory.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * This file is part of the PPI Framework.
+ *
+ * @copyright   Copyright (c) 2012 Paul Dragoonis <paul@ppi.io>
+ * @license     http://opensource.org/licenses/mit-license.php MIT
+ * @link        http://www.ppi.io
+ */
+
+namespace PPI\ServiceManager\Factory;
+
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\RouteCollection;
+use PPI\Module\Routing\Router;
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+/**
+ * Router Factory.
+ *
+ * @author     Vítor Brandão <vitor@ppi.io>
+ * @package    PPI
+ * @subpackage ServiceManager
+ */
+class RouterFactory implements FactoryInterface
+{
+    /**
+     * Create and return the router.
+     *
+     * @param  ServiceLocatorInterface    $serviceLocator
+     * @return \PPI\Module\Routing\Router
+     */
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        $routeCollection = new RouteCollection();
+        $requestContext  = new RequestContext();
+
+        $requestContext->fromRequest($serviceLocator->get('request'));
+        $routerOptions = array();
+
+        $router = new Router($requestContext, $routeCollection, $routerOptions);
+
+        $allRoutes = $serviceLocator->get('module.defaultListener')->getRoutes();
+        foreach ($allRoutes as $routes) {
+            $routeCollection->addCollection($routes);
+        }
+        $router->setRouteCollection($routeCollection);
+
+        return $router;
+    }
+}
