f07789f923e014d879d800d53b5fdfac4c6be0c3
Vítor Brandão <vitor@noiselabs.org>
2013-03-17 14:07:34 +0000

    Add Http\Response and Http\ResponseInterface
    Conflicts:
    	PPI/ServiceManager/Factory/ResponseFactory.php

---
+++ PPI/ServiceManager/Factory/ResponseFactory.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * This file is part of the PPI Framework.
+ *
+ * @copyright   Copyright (c) 2011-2013 Paul Dragoonis <paul@ppi.io>
+ * @license     http://opensource.org/licenses/mit-license.php MIT
+ * @link        http://www.ppi.io
+ */
+
+namespace PPI\ServiceManager\Factory;
+
+use PPI\Http\Response;
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+/**
+ * ServiceManager configuration for the HttpResponse component.
+ *
+ * @author     Vítor Brandão <vitor@ppi.io>
+ * @package    PPI
+ * @subpackage ServiceManager
+ */
+class ResponseFactory implements FactoryInterface
+{
+    /**
+     * Create and return a response instance.
+     *
+     * @param  ServiceLocatorInterface     $serviceLocator
+     * @return \PPI\Http\Response\Response
+     */
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        return new Response();
+    }
+}
