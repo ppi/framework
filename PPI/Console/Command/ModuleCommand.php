707346af818116ef2364ba0d438440fb7130675a
Vítor Brandão <vitor@noiselabs.org>
2013-04-06 01:52:17 +0100

    ModuleCommand now extends PPI\Console\Command\AbstractCommand

--- PPI/Console/Command/ModuleCommand.php
+++ PPI/Console/Command/ModuleCommand.php
@@ -1,14 +1,28 @@
 <?php
+/**
+ * This file is part of the PPI Framework.
+ *
+ * @copyright  Copyright (c) 2011-2013 Paul Dragoonis <paul@ppi.io>
+ * @license    http://opensource.org/licenses/mit-license.php MIT
+ * @link       http://www.ppi.io
+ */
 
 namespace PPI\Console\Command;
 
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class ModuleCommand extends Command
+/**
+ * Module Command.
+ *
+ * @author      Paul Dragoonis <paul@ppi.io>
+ * @author      Vítor Brandão <vitor@ppi.io> <vitor@noiselabs.org>
+ * @package     PPI
+ * @subpackage  Console
+ */
+class ModuleCommand extends AbstractCommand
 {
     protected $skeletonModuleDir;
     protected $modulesDir;
