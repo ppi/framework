42f214676a6d952cd8735cb0ade9edf53a51a21d
Vítor Brandão <vitor@noiselabs.org>
2013-04-06 01:51:16 +0100

    Add Console/Command/AbstractCommand.php

---
+++ PPI/Console/Command/AbstractCommand.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * This file is part of the PPI Framework.
+ *
+ * @copyright  Copyright (c) 2011-2013 Paul Dragoonis <paul@ppi.io>
+ * @license    http://opensource.org/licenses/mit-license.php MIT
+ * @link       http://www.ppi.io
+ */
+
+namespace PPI\Console\Command;
+
+use Symfony\Component\Console\Command\Command;
+
+/**
+ * Base class for all commands.
+ *
+ * @author      Vítor Brandão <vitor@ppi.io> <vitor@noiselabs.org>
+ * @package     PPI
+ * @subpackage  Console
+ */
+abstract class AbstractCommand extends Command
+{
+    // ...
+}
