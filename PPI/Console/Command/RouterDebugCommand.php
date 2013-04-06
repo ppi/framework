3d97471ae3fdbe469d6ad64551b244d4a38678ca
Vítor Brandão <vitor@noiselabs.org>
2013-04-06 01:54:48 +0100

    Add Console/Command/RouterDebugCommand.php

---
+++ PPI/Console/Command/RouterDebugCommand.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * This file is part of the PPI Framework.
+ *
+ * @copyright  Copyright (c) 2011-2013 Paul Dragoonis <paul@ppi.io>
+ * @license    http://opensource.org/licenses/mit-license.php MIT
+ * @link       http://www.ppi.io
+ */
+
+namespace PPI\Console\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\RouterDebugCommand as BaseRouterDebugCommand;
+
+/**
+ * A console command for retrieving information about routes
+ *
+ * @author      Vítor Brandão <vitor@ppi.io> <vitor@noiselabs.org>
+ * @package     PPI
+ * @subpackage  Console
+ */
+class RouterDebugCommand extends BaseRouterDebugCommand
+{
+
+}
