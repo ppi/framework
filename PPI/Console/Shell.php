ff83eaf903ea868849a97affe8b9fd4aa528f6f2
Vítor Brandão <vitor@noiselabs.org>
2013-04-06 01:54:08 +0100

    Add Console/Shell.php

---
+++ PPI/Console/Shell.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * This file is part of the PPI Framework.
+ *
+ * @copyright  Copyright (c) 2011-2013 Paul Dragoonis <paul@ppi.io>
+ * @license    http://opensource.org/licenses/mit-license.php MIT
+ * @link       http://www.ppi.io
+ */
+
+namespace PPI\Console;
+
+use Symfony\Component\Console\Shell as BaseShell;
+
+/**
+ * Shell.
+ *
+ * @author      Vítor Brandão <vitor@ppi.io> <vitor@noiselabs.org>
+ * @package     PPI
+ * @subpackage  Console
+ */
+class Shell extends BaseShell
+{
+    /**
+     * Returns the shell header.
+     *
+     * @return string The header string
+     */
+    protected function getHeader()
+    {
+        return <<<EOF
+<info>
+           _____   _____ |_|
+          / __  | /  __ | /|
+         | |__| || |__| || |
+         |  ___/ |  ___/ | |
+         | |     | |     |_|
+         |/      |/
+
+</info>
+EOF
+        .parent::getHeader();
+    }
+}
