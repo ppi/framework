4d9fb174b56b671cca65b3b7057079f6be0ea4b2
Vítor Brandão <vitor@noiselabs.org>
2013-04-07 04:08:13 +0100

    Add RouterMatchCommand

---
+++ PPI/Console/Command/RouterMatchCommand.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This file is part of the PPI Framework.
+ *
+ * @copyright  Copyright (c) 2011-2013 Paul Dragoonis <paul@ppi.io>
+ * @license    http://opensource.org/licenses/mit-license.php MIT
+ * @link       http://www.ppi.io
+ */
+
+namespace PPI\Console\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\RouterMatchCommand as BaseRouterMatchCommand;
+
+/**
+ * A console command to test route matching.
+ *
+ * @author      Vítor Brandão <vitor@ppi.io> <vitor@noiselabs.org>
+ * @package     PPI
+ * @subpackage  Console
+ */
+class RouterMatchCommand extends BaseRouterMatchCommand
+{
+}
