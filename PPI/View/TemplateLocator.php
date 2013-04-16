4cb030495d2ba3e8f7fb93c745f82d02ea7fe71e
Vítor Brandão <vitor@noiselabs.org>
2013-04-16 10:00:18 +0100

    View/TemplateLocator extends TemplateLocator from Symfony\FrameworkBundle instead of Component

--- PPI/View/TemplateLocator.php
+++ PPI/View/TemplateLocator.php
@@ -2,100 +2,24 @@
 /**
  * This file is part of the PPI Framework.
  *
- * @copyright  Copyright (c) 2012 Paul Dragoonis <paul@ppi.io>
+ * @copyright  Copyright (c) 2011-2013 Paul Dragoonis <paul@ppi.io>
  * @license    http://opensource.org/licenses/mit-license.php MIT
  * @link       http://www.ppi.io
  */
+
 namespace PPI\View;
 
-use PPI\View\TemplateReference,
-    Symfony\Component\Config\FileLocatorInterface,
-    Symfony\Component\Templating\TemplateReferenceInterface;
+use Symfony\Bundle\FrameworkBundle\Templating\Loader\TemplateLocator as BaseTemplateLocator;
 
 /**
  * The PPI Template Locator.
  *
- * @author     Fabien Potencier <fabien@symfony.com>
- * @author     Vítor Brandão <vitor@noiselabs.org>
+ * @author     Vítor Brandão <vitor@ppi.io> <vitor@noiselabs.org>
  * @package    PPI
  * @subpackage Templating
  */
-class TemplateLocator implements FileLocatorInterface
+class TemplateLocator extends BaseTemplateLocator
 {
-    /**
-     * @todo Add inline documentation.
-     *
-     * @var type
-     */
-    protected $locator;
-
-    /**
-     * @todo Add inline documentation.
-     *
-     * @var type
-     */
-    protected $cache;
-
-    /**
-     * Constructor.
-     *
-     * @param FileLocatorInterface $locator  A FileLocatorInterface instance
-     * @param string               $cacheDir The cache path
-     *
-     * @return void
-     */
-    public function __construct(FileLocatorInterface $locator, $cacheDir = null)
-    {
-        if (null !== $cacheDir && is_file($cache = $cacheDir.'/templates.php')) {
-            $this->cache = require $cache;
-        }
-
-        $this->locator = $locator;
-    }
-
-    /**
-     * Returns a full path for a given file.
-     *
-     * @param TemplateReferenceInterface $template A template
-     *
-     * @return string The full path for the file
-     */
-    protected function getCacheKey($template)
-    {
-        return $template->getLogicalName();
-    }
-
-    /**
-     * Returns a full path for a given file.
-     *
-     * @param TemplateReferenceInterface $template    A template
-     * @param string                     $currentPath Unused
-     * @param boolean                    $first       Unused
-     *
-     * @return string The full path for the file
-     *
-     * @throws \InvalidArgumentException When the template is not an instance of TemplateReferenceInterface
-     * @throws \InvalidArgumentException When the template file can not be found
-     */
-    public function locate($template, $currentPath = null, $first = true)
-    {
-        if (!$template instanceof TemplateReferenceInterface) {
-            throw new \InvalidArgumentException("The template must be an instance of TemplateReferenceInterface.");
-        }
-
-        $key = $this->getCacheKey($template);
-
-        if (isset($this->cache[$key])) {
-            return $this->cache[$key];
-        }
-
-        try {
-            return $this->cache[$key] = $this->locator->locate($template->getPath(), $currentPath);
-        } catch (\InvalidArgumentException $e) {
-            throw new \InvalidArgumentException(sprintf('Unable to find template "%s" : "%s".', $template, $e->getMessage()), 0, $e);
-        }
-    }
-
     /**
      * Returns the path to the views directory in the app dir.
      *
@@ -103,7 +27,7 @@ class TemplateLocator implements FileLocatorInterface
      */
     public function getAppPath()
     {
-        return $this->locator->appPath.DIRECTORY_SEPARATOR.TemplateReference::APP_VIEWS_DIRECTORY;
+        return $this->locator->getAppPath() . DIRECTORY_SEPARATOR . TemplateReference::APP_VIEWS_DIRECTORY;
     }
 
     /**
@@ -116,7 +40,7 @@ class TemplateLocator implements FileLocatorInterface
         $paths = $this->locator->getModulesPath();
 
         foreach (array_keys($paths) as $module) {
-            $paths[$module] .= DIRECTORY_SEPARATOR.TemplateReference::MODULE_VIEWS_DIRECTORY;
+            $paths[$module] .= DIRECTORY_SEPARATOR . TemplateReference::MODULE_VIEWS_DIRECTORY;
         }
 
         return $paths;
