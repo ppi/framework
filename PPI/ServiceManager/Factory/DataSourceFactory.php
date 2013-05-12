1c6d9a35edce9bbd305fa0f53d0b98a69efcf548
Paul Dragoonis <dragoonis@gmail.com>
2013-05-12 18:16:38 +0100

    [DataSource] Refactored and cleaned up how connections are loaded up.

--- PPI/ServiceManager/Factory/DataSourceFactory.php
+++ PPI/ServiceManager/Factory/DataSourceFactory.php
@@ -12,10 +12,12 @@ namespace PPI\ServiceManager\Factory;
 use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
 
+use PPI\DataSource\ConnectionManager;
+
 use PPI\DataSource\Connection\DoctrineDBAL as DoctrineDBALConnection;
 use PPI\DataSource\Connection\FuelPHP as FuelPHPConnection;
 use PPI\DataSource\Connection\Laravel as LaravelConnection;
-use PPI\DataSource\ConnectionManager;
+use PPI\DataSource\Connection\Monga as MongaConnection;
 
 /**
  * DataSource Factory.
@@ -35,35 +37,46 @@ class DataSourceFactory implements FactoryInterface
     public function createService(ServiceLocatorInterface $serviceLocator)
     {
 
-        $config = $serviceLocator->get('ApplicationConfig');
-        $allConnections = $libraryToConnMap = $laravelConns = $doctrineDBALConns = $fuelphpConns = array();
+        $config         = $serviceLocator->get('ApplicationConfig');
+        $allConnections = $libraryToConnMap = $configMap = array();
 
         if(isset($config['datasource']['connections'])) {
             foreach($config['datasource']['connections'] as $name => $conn) {
+
                 $allConnections[$name] = $conn;
-                if($conn['library'] === 'laravel') {
-                    $laravelConns[$name] = $conn;
+
+                switch($conn['library']) {
+
+                    case 'laravel':
+                        $configMap['laravel'][$name] = $conn;
+                        break;
+
+                    case 'doctrine_dbal':
+                        $configMap['doctrine_dbal'][$name] = $conn;
+                        break;
+
+                    case 'fuelphp':
+                        $configMap['fuelphp'][$name] = $conn;
+                        break;
+
+                    case 'monga':
+                        $configMap['monga'][$name] = $conn;
+                        break;
                 }
-                if($conn['library'] === 'doctrine_dbal') {
-                    $doctrineDBALConns[$name] = $conn;
-                }
-                if($conn['library'] === 'fuelphp') {
-                    $fuelphpConns[$name] = $conn;
-                }
             }
 
-            if(!empty($laravelConns)) {
-                $libraryToConnMap['laravel'] = new LaravelConnection($laravelConns);
+            if(isset($configMap['laravel']) && !empty($configMap['laravel'])) {
+                $libraryToConnMap['laravel'] = new LaravelConnection($configMap['laravel']);
             }
-
-            if(!empty($doctrineDBALConns)) {
-                $libraryToConnMap['doctrine_dbal'] = new DoctrineDBALConnection($doctrineDBALConns);
+            if(isset($configMap['doctrine_dbal']) && !empty($configMap['doctrine_dbal'])) {
+                $libraryToConnMap['doctrine_dbal'] = new DoctrineDBALConnection($configMap['doctrine_dbal']);
             }
-
-            if(!empty($fuelphpConns)) {
-                $libraryToConnMap['fuelphp'] = new FuelPHPConnection($fuelphpConns);
+            if(isset($configMap['fuelphp']) && !empty($configMap['fuelphp'])) {
+                $libraryToConnMap['fuelphp'] = new FuelPHPConnection($configMap['fuelphp']);
             }
-
+            if(isset($configMap['monga']) && !empty($configMap['monga'])) {
+                $libraryToConnMap['monga'] = new MongaConnection($$configMap['monga']);
+            }
         }
 
         return new ConnectionManager($allConnections, $libraryToConnMap);
