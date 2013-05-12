e5520cc1fcec5e6746d1c027bd7a3dd1a1802994
Paul Dragoonis <dragoonis@gmail.com>
2013-05-12 17:13:58 +0100

    [DataSource] Cleanup of the DoctrineDBAl and FuelPHP Connection classes

--- PPI/DataSource/Connection/DoctrineDBAL.php
+++ PPI/DataSource/Connection/DoctrineDBAL.php
@@ -8,22 +8,25 @@ use Doctrine\DBAL\DriverManager;
 class DoctrineDBAL implements ConnectionInferface
 {
 
-	protected $conns;
+    protected $config = array();
+	protected $conns = array();
 
-    public function __construct(array $connections)
+    public function __construct(array $config)
     {
-    	foreach($connections as $name => $config) {
-    		$conns[$name] = DriverManager::getConnection($this->normaliseConfigKeys($config));
-    	}
-		$this->conns = $conns;
+        $this->config = $config;
     }
 
     public function getConnectionByName($name)
     {
 
-    	if(!isset($this->conns[$name])) {
+    	if(!isset($this->config[$name])) {
     		throw new \Exception('No doctrine dbal connection found named: ' . $name);
     	}
+
+        if (!isset($this->connections[$name])) {
+            $this->conns[$name] = DriverManager::getConnection($this->normaliseConfigKeys($config));
+        }
+        
     	return $this->conns[$name];
     }
 
