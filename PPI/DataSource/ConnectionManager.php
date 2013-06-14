52939f0a73caf941e613946beda03b8fafb56539
Paul Dragoonis <dragoonis@gmail.com>
2013-06-14 01:22:06 +0100

    [DataSource] Making class autoloading a lazy-loaded event so that composer dependencies don't flare up. Also cleaned up and simplified the DataSourceFactory.

--- PPI/DataSource/ConnectionManager.php
+++ PPI/DataSource/ConnectionManager.php
@@ -5,12 +5,13 @@ namespace PPI\DataSource;
 class ConnectionManager
 {
     protected $connections;
+    protected $connectionClassMap;
     protected $libraryToConnMap;
 
-    public function __construct($connections, $libraryToConnMap)
+    public function __construct($connections, $connectionClassMap)
     {
-        $this->libraryToConnMap = $libraryToConnMap;
-        $this->connections      = $connections;
+        $this->connections        = $connections;
+        $this->connectionClassMap = $connectionClassMap;
     }
 
     public function getConnection($name)
@@ -19,10 +20,33 @@ class ConnectionManager
             throw new \Exception('Unable to locate connection by name: ' . $name);
         }
 
+        // Who's the vendor?
         $library = $this->connections[$name]['library'];
-        $conn    = $this->libraryToConnMap[$library]->getConnectionByName($name);
 
-        return $conn;
+        // Have we been here before?
+        if(!isset($this->libraryToConnMap[$library])) {
+
+            // Identify the vendor connection classname from the classmap
+            $connectionClass = $this->connectionClassMap[$library];
+
+            // Create the connection object and keep it for future use
+            $this->libraryToConnMap[$library] = new $connectionClass($this->getConnectionsByLibrary($library));
+
+        }
+
+        return $this->libraryToConnMap[$library]->getConnectionByName($name);
+    }
+
+    public function getConnectionsByLibrary($library)
+    {
+        $conns = array();
+        foreach($this->connections as $name => $config)
+        {
+            if($config['library'] === $library) {
+                $conns[$name] = $config;
+            }
+        }
+        return $conns;
     }
 
 }
