cc921bec903939de69aa4cbd7c638247dd271386
Paul Dragoonis <dragoonis@gmail.com>
2013-06-16 01:00:50 +0100

    Removing unnceeded dependencies from ppi\log

--- PPI/Log/Logger.php
+++ PPI/Log/Logger.php
@@ -10,47 +10,11 @@
 namespace PPI\Log;
 
 use Monolog\Logger as BaseLogger;
-use Symfony\Component\HttpKernel\Log\LoggerInterface;
-use Symfony\Component\HttpKernel\Log\DebugLoggerInterface;
 
 /**
  * Logger is PSR-3 compliant logger based on Monolog\Logger.
  *
  * @author Vítor Brandão <vitor@ppi.io>
+ * @author Paul Dragooni <paul@ppi.io>
  */
-class Logger extends BaseLogger implements LoggerInterface, DebugLoggerInterface
-{
-    /**
-     * @see Symfony\Component\HttpKernel\Log\DebugLoggerInterface
-     */
-    public function getLogs()
-    {
-        if ($logger = $this->getDebugLogger()) {
-            return $logger->getLogs();
-        }
-    }
-
-    /**
-     * @see Symfony\Component\HttpKernel\Log\DebugLoggerInterface
-     */
-    public function countErrors()
-    {
-        if ($logger = $this->getDebugLogger()) {
-            return $logger->countErrors();
-        }
-    }
-
-    /**
-     * Returns a DebugLoggerInterface instance if one is registered with this logger.
-     *
-     * @return DebugLoggerInterface A DebugLoggerInterface instance or null if none is registered
-     */
-    private function getDebugLogger()
-    {
-        foreach ($this->handlers as $handler) {
-            if ($handler instanceof DebugLoggerInterface) {
-                return $handler;
-            }
-        }
-    }
-}
+class Logger extends BaseLogger {}
