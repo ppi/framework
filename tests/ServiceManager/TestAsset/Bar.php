f0154e1e5d1400ec80d9f9df1b2aa7a9eb791624
Vítor Brandão <vitor@noiselabs.org>
2015-04-11 23:36:15 +0100

    Add tests/Router/RoutePluginManagerTest.php and tests/ServiceManager/TestAsset/*

---
+++ tests/ServiceManager/TestAsset/Bar.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * This file is part of the PPI Framework.
+ *
+ * @copyright  Copyright (c) 2011-2015 Paul Dragoonis <paul@ppi.io>
+ * @license    http://opensource.org/licenses/mit-license.php MIT
+ *
+ * @link       http://www.ppi.io
+ */
+
+namespace PPI\FrameworkTest\ServiceManager\TestAsset;
+
+/**
+ * Class Bar.
+ *
+ * @author Vítor Brandão <vitor@ppi.io>
+ */
+class Bar
+{
+    /**
+     * @param array $foo
+     */
+    public function __construct(array $foo = null)
+    {
+        if (null === $foo) {
+            throw new \RuntimeException();
+        }
+    }
+}
